99a7bd716249b34c797fac5c471a9e5fc8626a7f
SHOGO180 <s.uenaka@180-inc.com>
2024-05-09 16:53:31 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// fetch = 取り出す
+async function fetchUserData() { 
+  const username = prompt('GitHub ユーザー名を入力してください:');
+  const userDataContainer = document.querySelector('#userDataContainer');
+
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const userData = await response.json();
+
+    userDataContainer.innerHTML = `
+    <p>ユーザー名：${userData.login}</p>
+    <p>名前：${userData.name}</p>
+    <p>フォロワー数：${userData.followers}</p>
+    `;
+  }
+  catch(error) {
+    userDataContainer.innerHTML = `
+    
+    <p style="color: red">エラーが発生しました。</p>
+    `;
+  }
+}
